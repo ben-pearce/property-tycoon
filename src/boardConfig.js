c05a0ed6e93bd50841e6381417b7ed14b839e335
Ben <ben@solero.me>
2020-02-05 19:59:56 +0000

    Board layout JavaScript object
    From https://canvas.sussex.ac.uk/courses/9097/files/1177197?module_item_id=684274

---
+++ src/boardConfig.js
@@ -0,0 +1,292 @@
+module.exports = [
+	{
+		id: 1,
+		name: "Go",
+		buy: false,
+		action: "Collect \u00a3200"
+	},
+	{
+		id: 2,
+		name: "Crapper Street",
+		buy: true,
+		group: "Brown",
+		cost: 60,
+		rent: [2, 10, 30, 90, 160, 250]
+	},
+	{
+		id: 3,
+		name: "Pot Luck",
+		buy: false,
+		action: "Take card"
+	},
+	{
+		id: 4,
+		name: "Gangsters Paradise",
+		buy: true,
+		group: "Brown",
+		cost: 60,
+		rent: [4, 20, 60, 180, 320, 450]
+	},
+	{
+		id: 5,
+		name: "Income Tax",
+		buy: false,
+		action: "Pay \u00a3200"
+	},
+	{
+		id: 6,
+		name: "Brighton Station",
+		buy: true,
+		group: "Station",
+		cost: 200
+	},
+	{
+		id: 7,
+		name: "Weeping Angel",
+		buy: true,
+		group: "Blue",
+		cost: 100,
+		rent: [6, 30, 90, 270, 400, 550]
+	},
+	{
+		id: 8,
+		name: "Opportunity Knocks",
+		buy: false,
+		action: "Take card"
+	},
+	{
+		id: 9,
+		name: "Potts Avenue",
+		buy: true,
+		group: "Blue",
+		cost: 100,
+		rent: [6, 30, 90, 270, 400, 550]
+	},
+	{
+		id: 10,
+		name: "Nardole Drive",
+		buy: true,
+		group: "Blue",
+		cost: 120,
+		rent: [8, 40, 100, 300, 450, 600]
+	},
+	{
+		id: 11,
+		name: "Jail/Just visiting",
+		buy: false,
+		action: ""
+	},
+	{
+		id: 12,
+		name: "Skywalker Drive",
+		buy: true,
+		group: "Purple",
+		cost: 140,
+		rent: [10, 50, 150, 450, 625, 750]
+	},
+	{
+		id: 13,
+		name: "Tesla Power Co",
+		buy: true,
+		group: "Utilities",
+		cost: 150
+	},
+	{
+		id: 14,
+		name: "Wookie Hole",
+		buy: true,
+		group: "Purple",
+		cost: 140,
+		rent: [10, 50, 150, 450, 625, 750]
+	},
+	{
+		id: 15,
+		name: "Rey Lane",
+		buy: true,
+		group: "Purple",
+		cost: 160,
+		rent: [12, 60, 180, 500, 700, 900]
+	},
+	{
+		id: 16,
+		name: "Hove Station",
+		buy: true,
+		group: "Station",
+		cost: 200
+	},
+	{
+		id: 17,
+		name: "Cooper Drive",
+		buy: true,
+		group: "Orange",
+		cost: 180,
+		rent: [14, 70, 200, 550, 750, 950]
+	},
+	{
+		id: 18,
+		name: "Pot Luck",
+		buy: false,
+		action: "Take card"
+	},
+	{
+		id: 19,
+		name: "Wolowitz Street",
+		buy: true,
+		group: "Orange",
+		cost: 180,
+		rent: [14, 70, 200, 550, 750, 950]
+	},
+	{
+		id: 20,
+		name: "Penny Lane",
+		buy: true,
+		group: "Orange",
+		cost: 200,
+		rent: [16, 80, 220, 600, 800, 1000]
+	},
+	{
+		id: 21,
+		name: "Free Parking",
+		buy: false,
+		action: "Collect fines"
+	},
+	{
+		id: 22,
+		name: "Yue Fei Square",
+		buy: true,
+		group: "Red",
+		cost: 220,
+		rent: [18, 90, 250, 700, 875, 1050]
+	},
+	{
+		id: 23,
+		name: "Opportunity Knocks",
+		buy: false,
+		action: "Take card"
+	},
+	{
+		id: 24,
+		name: "Mulan Rouge",
+		buy: true,
+		group: "Red",
+		cost: 220,
+		rent: [18, 90, 250, 700, 875, 1050]
+	},
+	{
+		id: 25,
+		name: "Han Xin Gardens",
+		buy: true,
+		group: "Red",
+		cost: 240,
+		rent: [20, 100, 300, 750, 925, 1100]
+	},
+	{
+		id: 26,
+		name: "Falmer Station",
+		buy: true,
+		group: "Station",
+		cost: 200
+	},
+	{
+		id: 27,
+		name: "Kirk Close",
+		buy: true,
+		group: "Yellow",
+		cost: 260,
+		rent: [22, 110, 330, 800, 975, 1150]
+	},
+	{
+		id: 28,
+		name: "Picard Avenue",
+		buy: true,
+		group: "Yellow",
+		cost: 260,
+		rent: [22, 110, 330, 800, 975, 1150]
+	},
+	{
+		id: 29,
+		name: "Edison Water",
+		buy: true,
+		group: "Utilities",
+		cost: 150
+	},
+	{
+		id: 30,
+		name: "Crusher Creek",
+		buy: true,
+		group: "Yellow",
+		cost: 280,
+		rent: [22, 120, 360, 850, 1025, 1200]
+	},
+	{
+		id: 31,
+		name: "Go to Jail",
+		buy: false,
+		action: ""
+	},
+	{
+		id: 32,
+		name: "Sirat Mews",
+		buy: true,
+		group: "Green",
+		cost: 300,
+		rent: [26, 130, 390, 900, 1100, 1275]
+	},
+	{
+		id: 33,
+		name: "Ghengis Crescent",
+		buy: true,
+		group: "Green",
+		cost: 300,
+		rent: [26, 130, 390, 900, 1100, 1275]
+	},
+	{
+		id: 34,
+		name: "Pot Luck",
+		buy: false,
+		action: "Take card"
+	},
+	{
+		id: 35,
+		name: "Ibis Close",
+		buy: true,
+		group: "Green",
+		cost: 320,
+		rent: [28, 150, 450, 1000, 1200, 1400]
+	},
+	{
+		id: 36,
+		name: "Lewes Station",
+		buy: true,
+		group: "Station",
+		cost: 200
+	},
+	{
+		id: 37,
+		name: "Opportunity Knocks",
+		buy: false,
+		action: ""
+	},
+	{
+		id: 38,
+		name: "Hawking Way",
+		buy: true,
+		group: "Deep blue",
+		cost: 350,
+		rent: [35, 175, 500, 1100, 1300, 1500]
+	},
+	{
+		id: 39,
+		name: "Super Tax",
+		buy: false,
+		action: "Pay \u00a3100"
+	},
+	{
+		id: 40,
+		name: "Turing Heights",
+		buy: true,
+		group: "Deep blue",
+		cost: 400,
+		rent: [50, 200, 600, 1400, 1700, 2000]
+	}
+];
