0d46f59f65847bf16d689b88f002954f3d94a3cd
Mera898 <52925779+Mera898@users.noreply.github.com>
2020-02-25 19:21:35 +0400

    Add files via upload

--- src/tycoon/bank.js
+++ src/tycoon/bank.js
@@ -1,8 +1,28 @@
+import Phaser from "phaser";
+class Bank extends Phaser.GameObjects.Sprite{
+	constructor(board) {
+        super(board.scene,0,0,"bank");
 
-class Bank {
-	constructor() {
-		
-	}
+        this.board = board;
+        this.scene = board.scene;
+        this.spent = 0;
+        this.bank_money = 50000;
+
+    }
+    deposit(sum) {
+        this.bank_money += sum;
+    }
+
+
+    withdraw(sum) {
+        this.spent = sum;
+        this.bank_money -= sum;
+
+    }
+
+
+
+
 }
 
 module.exports = Bank;
