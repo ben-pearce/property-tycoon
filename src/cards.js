209fba58be846e9308dd17c1f2c816799df1145b
Ben <ben@solero.me>
2020-02-27 15:49:10 +0000

    Create cards file & represent cards as a JavaScript object

---
+++ src/cards.js
@@ -0,0 +1,132 @@
+module.exports = {
+	potluck: [
+		{
+			"name": "You inherit \u00a3100",
+			"action": "Bank pays player \u00a3100"
+		},
+		{
+			"name": "You have won 2nd prize in a beauty contest, collect \u00a320",
+			"action": "Bank pays player \u00a320"
+		},
+		{
+			"name": "Go back to Crapper Street",
+			"action": "Player token moves backwards to Crapper Street"
+		},
+		{
+			"name": "Student loan refund. Collect \u00a320",
+			"action": "Bank pays player \u00a320"
+		},
+		{
+			"name": "Bank error in your favour. Collect \u00a3200",
+			"action": "Bank pays player \u00a3200"
+		},
+		{
+			"name": "Pay bill for text books of \u00a3100",
+			"action": "Player pays \u00a3100 to the bank"
+		},
+		{
+			"name": "Mega late night taxi bill pay \u00a350",
+			"action": "Player pays \u00a350 to the bank"
+		},
+		{
+			"name": "Advance to go",
+			"action": "Player moves forwards to GO"
+		},
+		{
+			"name": "From sale of Bitcoin you get \u00a350",
+			"action": "Bank pays player \u00a350"
+		},
+		{
+			"name": "Pay a \u00a310 fine or take opportunity knocks",
+			"action": "If fine paid, player puts \u00a310 on free parking"
+		},
+		{
+			"name": "Pay insurance fee of \u00a350",
+			"action": "Player puts \u00a350 on free parking"
+		},
+		{
+			"name": "Savings bond matures, collect \u00a3100",
+			"action": "Bank pays \u00a3100 to the player"
+		},
+		{
+			"name": "Go to jail. Do not pass GO, do not collect \u00a3200",
+			"action": "As the card says"
+		},
+		{
+			"name": "Received interest on shares of \u00a325",
+			"action": "Bank pays player \u00a325"
+		},
+		{
+			"name": "It's your birthday. Collect \u00a310 from each player",
+			"action": "Player receives \u00a310 from each player"
+		},
+		{
+			"name": "Get out of jail free",
+			"action": "Retained by the player until needed. No resale or trade value"
+		}],
+	opportunity: [
+		{
+			"name": "Bank pays you divided of \u00a350",
+			"action": "Bank pays player \u00a350"
+		},
+		{
+			"name": "You have won a lip sync battle. Collect \u00a3100",
+			"action": "Bank pays player \u00a3100"
+		},
+		{
+			"name": "Advance to Turing Heights",
+			"action": "Player token moves forwards to Turing Heights"
+		},
+		{
+			"name": "Advance to Han Xin Gardens. If you pass GO, collect \u00a3200",
+			"action": "Player moves token"
+		},
+		{
+			"name": "Fined \u00a315 for speeding",
+			"action": "Player puts \u00a315 on free parking"
+		},
+		{
+			"name": "Pay university fees of \u00a3150",
+			"action": "Player pays \u00a3150 to the bank"
+		},
+		{
+			"name": "Take a trip to Hove station. If you pass GO collect \u00a3200",
+			"action": "Player moves token"
+		},
+		{
+			"name": "Loan matures, collect \u00a3150",
+			"action": "Bank pays \u00a3150 to the player"
+		},
+		{
+			"name": "You are assessed for repairs, \u00a340/house, \u00a3115/hotel",
+			"action": "Player pays money to the bank"
+		},
+		{
+			"name": "Advance to GO",
+			"action": "Player moves token"
+		},
+		{
+			"name": "You are assessed for repairs, \u00a325/house, \u00a3100/hotel",
+			"action": "Player pays money to the bank"
+		},
+		{
+			"name": "Go back 3 spaces",
+			"action": "Player moves token"
+		},
+		{
+			"name": "Advance to Skywalker Drive. If you pass GO collect \u00a3200",
+			"action": "Player moves token"
+		},
+		{
+			"name": "Go to jail. Do not pass GO, do not collect \u00a3200",
+			"action": "As the card says"
+		},
+		{
+			"name": "Drunk in charge of a skateboard. Fine \u00a320",
+			"action": "Player puts \u00a320 on free parking"
+		},
+		{
+			"name": "Get out of jail free",
+			"action": "Retained by the player until needed. No resale or trade value"
+		}]
+};
