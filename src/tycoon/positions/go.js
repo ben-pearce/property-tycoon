04c1d7ef1f62fdbef127da4d1e1f194f5a07b779
Ben <ben@solero.me>
2020-02-27 15:47:33 +0000

    Create & move files for position classes
    Moved to their own module
    
    Created additional files for all future classes

---
+++ src/tycoon/positions/go.js
@@ -0,0 +1,17 @@
+import Position from "./position";
+
+class Go extends Position {
+	constructor(game, options) {
+		super(game, options);
+	}
+
+	onLanded() {
+
+	}
+
+	onPassed() {
+
+	}
+}
+
+export default Go;
