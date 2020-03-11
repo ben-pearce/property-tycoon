bfd71bda87d434da8df207d2625f8f56cb039941
Ben <ben.pearce@sky.com>
2020-03-11 00:22:33 +0000

    Replace spaces with tabs

--- src/tycoon/bank.js
+++ src/tycoon/bank.js
@@ -1,28 +1,21 @@
 import Phaser from "phaser";
 class Bank extends Phaser.GameObjects.Sprite{
 	constructor(board) {
-        super(board.scene,0,0,"bank");
+		super(board.scene,0,0,"bank");
+		this.board = board;
+		this.scene = board.scene;
+		this.spent = 0;
+		this.bankMoney = 50000;
+	}
 
-        this.board = board;
-        this.scene = board.scene;
-        this.spent = 0;
-        this.bank_money = 50000;
+	deposit(sum) {
+		this.bankMoney += sum;
+	}
 
-    }
-    deposit(sum) {
-        this.bank_money += sum;
-    }
-
-
-    withdraw(sum) {
-        this.spent = sum;
-        this.bank_money -= sum;
-
-    }
-
-
-
-
+	withdraw(sum) {
+		this.spent = sum;
+		this.bankMoney -= sum;
+	}
 }
 
 module.exports = Bank;
