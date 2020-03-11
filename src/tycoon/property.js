9bf8396e371e6afacac0269bc6dca58f178e6621
Ben <ben@solero.me>
2020-03-11 00:29:35 +0000

    Change incorrectly assigned local variables into class variables

--- src/tycoon/property.js
+++ src/tycoon/property.js
@@ -24,27 +24,30 @@ class Property extends Phaser.GameObjects.Rectangle {
 		this.tile = tile;
 
 		this.setStrokeStyle(3, 0x00000);
-        this.setOrigin(0);
-        var house = 0;
-        var housefull = false;
-        var hotelNo = 0;
-    }
-    //With every successful upgrade we will add a house/hotel to the board.
-    upgrade() {
-        if ((this.house == 4)&&(this.housefull==false)) {
-            //you need to pay to upgrade into hotel.
-	    
-            this.hotelNo = 1;
-            this.housefull = true;
-        }
-        if (this.house < 4) {
-            this.house++;
-        }
-        if ((this.housefull == true)&&this.hotelNo<4) {
-            hotelNo++;
-        }
-        if (this.hotelNo == 4) {
-            //Upgrade Completed
-        }
-    }
+		this.setOrigin(0);
+		
+		this.house = 0;
+		this.housefull = false;
+		this.hotelNo = 0;
+	}
+
+	//With every successful upgrade we will add a house/hotel to the board.
+	upgrade() {
+		if ((this.house == 4)&&(this.housefull==false)) {
+			//you need to pay to upgrade into hotel.
+			this.hotelNo = 1;
+			this.housefull = true;
+		}
+		if (this.house < 4) {
+			this.house++;
+		}
+		if ((this.housefull == true)&&this.hotelNo<4) {
+			this.hotelNo++;
+		}
+		if (this.hotelNo == 4) {
+			//Upgrade Completed
+		}
+	}
 }
+
+export default Property;
