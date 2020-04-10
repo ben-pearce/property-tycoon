b1594655c023171ef5a748c20031cdff6c1393b9
Ben <ben.pearce@sky.com>
2020-04-10 18:53:13 +0100

    Move text style objects to their own module

---
+++ src/styles.js
@@ -0,0 +1,27 @@
+const TileTextStyle = {
+	fontFamily: "Sans",
+	color: "#000000", 
+	fontSize: "10px",
+	align: "center"
+};
+
+const CashTextStyle = {
+	fontFamily: "Arial",
+	color: "#000000", 
+	fontSize: "10px",
+	align: "center"
+};
+
+
+const CashUpdateStyle = {
+	fontFamily: "Arial",
+	fontStyle: "bold",
+	fontSize: "15px",
+	align: "center"
+};
+
+export {
+	TileTextStyle,
+	CashTextStyle,
+	CashUpdateStyle
+};
