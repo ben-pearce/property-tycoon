8dab704a3185531e65814ab488959377071709af
Ben <ben@solero.me>
2020-04-11 02:10:25 +0100

    Add `getPlayerJailXY()` and `getPlayerVisitingXY()` to jail tile class

--- src/tycoon/tiles/jail.js
+++ src/tycoon/tiles/jail.js
@@ -7,14 +7,46 @@ class Jail extends Tile {
 
 		this.text.setVisible(false);
 
-		let jailPlatform = new Phaser.GameObjects.Rectangle(
+		this.jailPlatform = new Phaser.GameObjects.Rectangle(
 			this.board.scene, 
 			this.x, this.y, 
 			this.background.height * 0.70, this.background.width * 0.70,
-			0xf7941d);
-		jailPlatform.setOrigin(0);
-		jailPlatform.setStrokeStyle(3, 0x000000);
-		this.add(jailPlatform);
+			0xf7941d
+		);
+		this.jailPlatform.setOrigin(0);
+		this.jailPlatform.setStrokeStyle(3, 0x000000);
+		this.add(this.jailPlatform);
+	}
+
+	getPlayerJailXY() {
+		this.posRange = [
+			[-0.3, 0.4],
+			[0, 0],
+			[0, 0.4],
+			[0, 0.2],
+			[-0.3, 0],
+			[-0.3, 0.2]
+		];
+
+		return super.getPlayerXY();
+	}
+
+	getPlayerVisitingXY() {
+		this.posRange = [
+			[-0.3, -0.3],
+			[0.3, -0.3],
+			[0.3, 0.3],
+			[-0.1, -0.3],
+			[0.1, -0.3],
+			[0.3, 0],
+			
+		];
+
+		return super.getPlayerXY();
+	}
+
+	getPlayerXY() {
+		return this.getPlayerVisitingXY();
 	}
 }
 
