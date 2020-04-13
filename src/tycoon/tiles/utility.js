18de4fd74752112557fffd101bbad52163911515
Ben <ben@solero.me>
2020-04-14 00:50:19 +0100

    Add graphic to utility and opportunity tiles

--- src/tycoon/tiles/utility.js
+++ src/tycoon/tiles/utility.js
@@ -1,10 +1,18 @@
+import Phaser from "phaser";
 import Purchasable from "./purchasable";
 
 class Utility extends Purchasable {
 	constructor(game, options) {
 		super(game, options);
 
+		let graphic = new Phaser.GameObjects.Sprite(
+			this.scene, 
+			this.background.x + (this.background.width / 2), 
+			this.background.y + (this.background.height / 2) + 10, 
+			"tiles", options.graphic);
+
 		this.text.setY(this.y + 10);
+		this.add(graphic);
 	}
 }
 
