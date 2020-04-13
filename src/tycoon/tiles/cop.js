7bcab07f55dc327e99e0d6a0643edf97f8a67996
Ben <ben@solero.me>
2020-04-14 00:48:47 +0100

    Add graphic to cop tile

--- src/tycoon/tiles/cop.js
+++ src/tycoon/tiles/cop.js
@@ -1,10 +1,19 @@
-//import Phaser from "phaser";
+import Phaser from "phaser";
 import Tile from "./tile";
 
 
 class Cop extends Tile {
 	constructor(game, options) {
 		super(game, options);
+
+		let graphic = new Phaser.GameObjects.Sprite(
+			this.scene, 
+			this.background.x + (this.background.width / 2), 
+			this.background.y + (this.background.height / 2), 
+			"tiles", "cop");
+		
+		this.text.setVisible(false);
+		this.add(graphic);
 	}
     
 	onLanded(player) {
