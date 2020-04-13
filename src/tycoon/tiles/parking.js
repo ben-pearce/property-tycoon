e3e83bcd23ddf9a87eb623942d3144e7ed23da43
Ben <ben.pearce@sky.com>
2020-04-14 00:48:19 +0100

    Add graphic to free parking

--- src/tycoon/tiles/parking.js
+++ src/tycoon/tiles/parking.js
@@ -1,8 +1,18 @@
+import Phaser from "phaser";
 import Tile from "./tile";
 
 class Parking extends Tile {
 	constructor(game, options) {
 		super(game, options);
+
+		let graphic = new Phaser.GameObjects.Sprite(
+			this.scene, 
+			this.background.x + (this.background.width / 2), 
+			this.background.y + (this.background.height / 2), 
+			"tiles", "parking");
+		
+		this.text.setVisible(false);
+		this.add(graphic);
 	}
 
 	onLanded(player) {
