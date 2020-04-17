2ad712dd7e34890535f53e7d2a9eb91ee067d211
Ben <ben.pearce@sky.com>
2020-04-17 03:27:41 +0100

    Updates to `Bank` class

--- src/tycoon/bank.js
+++ src/tycoon/bank.js
@@ -1,21 +1,53 @@
 import Phaser from "phaser";
-class Bank extends Phaser.GameObjects.Sprite{
-	constructor(board) {
-		super(board.scene,0,0,"bank");
-		this.board = board;
-		this.scene = board.scene;
+
+class Bank extends Phaser.Events.EventEmitter {
+	/**
+	 * This class is the internet representation 
+	 * of the game bank.
+	 * 
+	 * Responsible for keeping track of amount of cash
+	 * bank has spent and received.
+	 * 
+	 * It is also an EventEmitter, which registers two events:
+	 * 
+	 * 	- "deposit" - Fired when bank.deposit() is called and passes
+	 * the sum deposited as the parameter to the event callback.
+	 * 	- "withdraw" - Fired when bank.withdraw() is called and passed
+	 * the sum withdrawn ad the parameter to the event callback.
+	 * 
+	 * @param {GameManager} game The game manager instance this belongs to. 
+	 */
+	constructor(game) {
+		super();
+
+		this.game = game;
+		this.scene = game.scene;
 		this.spent = 0;
-		this.bankMoney = 50000;
+		this.cash = 0;
 	}
 
+	/**
+	 * Deposit cash into this bank.
+	 * 
+	 * @param {Integer} sum The amount to deposit.
+	 */
 	deposit(sum) {
-		this.bankMoney += sum;
+		this.cash += sum;
+
+		this.emit("deposit", sum);
 	}
 
+	/**
+	 * Withdraw cash from this bank.
+	 * 
+	 * @param {Integer} sum The amount to withdraw.
+	 */
 	withdraw(sum) {
-		this.spent = sum;
-		this.bankMoney -= sum;
+		this.spent += sum;
+		this.cash -= sum;
+
+		this.emit("withdraw", sum);
 	}
 }
 
-module.exports = Bank;
+export default Bank;
