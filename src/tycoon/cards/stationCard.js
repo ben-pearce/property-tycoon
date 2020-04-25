ff107738bc16172233ffa41f10f7be2371eb8992
Ben <ben.pearce@sky.com>
2020-04-25 15:00:15 +0100

    rentableCard and stationCard inherit purchasableCard

--- src/tycoon/cards/stationCard.js
+++ src/tycoon/cards/stationCard.js
@@ -1,18 +1,16 @@
 import Phaser from "phaser";
 import PurchasableCard from "./purchasableCard";
-import RoundRectangle from "phaser3-rex-plugins/plugins/roundrectangle";
 import {CardStyle} from "../../styles";
 
 /**
- * Represents a graphical rentable tile card.
+ * Represents a graphical station tile card,
  * 
  * @extends Card
  * @memberof Cards
  */
-class RentableCard extends PurchasableCard {
+class StationCard extends PurchasableCard {
 	/**
-	 * Creates a colored box to represent rentable group,
-	 * a title and price.
+	 * Creates a title and price.
 	 * 
 	 * @param {GameManager} game The game instance this belongs to.
 	 * @param {Tile} tile The tile instance to observe.
@@ -20,17 +18,14 @@ class RentableCard extends PurchasableCard {
 	 */
 	constructor(game, tile, player) {
 		super(game, tile, player);
-		
-		let color = new RoundRectangle(this.scene, 0, -250, 380, 80, 15, tile.color);
-		color.setStrokeStyle(3, 0x000000);
 
 		let title = new Phaser.GameObjects.Text(this.scene, 0, -250, tile.name, CardStyle);
-		title.setPosition(title.x - (title.width /2), title.y - (title.height / 2)).setStyle({color: "#FFFFFF"});
+		title.setPosition(title.x - (title.width /2), title.y - (title.height / 2));
 
 		let price = new Phaser.GameObjects.Text(this.scene, -190, -190, `Price £${tile.cost}`, CardStyle);
         
-		this.add([color, title, price]);
+		this.add([title, price]);
 	}
 }
 
-export default RentableCard;
+export default StationCard;
